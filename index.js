53e49b3f57ca04e139d198e5438ef818c332a0a3
waterval <luukvla@hotmail.com>
2020-01-12 14:02:04 +0100

    revert "removed limiter"
    This reverts commit 3c33a49cb3012ab172f87d0fe6b9d804d5b3f6af.

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ const uploader = multer({
     }
 });
 
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 3,
+    message:
+        "Too many images uploaded from this IP, please try again in 5 minutes"
+});
+
 app.use(express.static("./public"));
 
 app.use(require("body-parser").json());
@@ -44,7 +51,7 @@ app.get("/images", function(request, response) {
         });
 });
 
-app.post("/upload", uploader.single("file"), s3.upload, function(
+app.post("/upload", limiter, uploader.single("file"), s3.upload, function(
     request,
     response
 ) {
